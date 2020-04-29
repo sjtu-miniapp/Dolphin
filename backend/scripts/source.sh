b9b9e0f91fe2ee6c121bf8f15e170df880f48fa8
blasi <blasi@vip.qq.com>
2020-04-29 19:19:27 +0800

    2390329ut803y4gh893497gg23899-2

--- backend/scripts/source.sh
+++ backend/scripts/source.sh
@@ -1,5 +1,6 @@
-#go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
-#protoc lorem.proto --go_out=plugins=grpc:.
+# lines
+# go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+# protoc lorem.proto --go_out=plugins=grpc:.
 export ALL_PROXY=
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
