f20aaf5cf91ab0cb6006ca4148dba8688615ab27
blasi <blasi@vip.qq.com>
2020-05-06 12:25:54 +0800

    .

--- backend/service/group/srv/impl/impl.go
+++ backend/service/group/srv/impl/impl.go
@@ -11,6 +11,26 @@ type Group struct {
 	SqlDb *sql.DB
 }
 
+func (g Group) GetGroupByUserId(ctx context.Context, request *pb.GetGroupByUserIdRequest, response *pb.GetGroupByUserIdResponse) error {
+	panic("implement me")
+}
+
+func (g Group) CreateGroup(ctx context.Context, request *pb.CreateGroupRequest, response *pb.CreateGroupResponse) error {
+	panic("implement me")
+}
+
+func (g Group) AddUser(ctx context.Context, request *pb.AddUserRequest, response *pb.AddUserResponse) error {
+	panic("implement me")
+}
+
+func (g Group) UpdateGroup(ctx context.Context, request *pb.UpdateGroupRequest, response *pb.UpdateGroupResponse) error {
+	panic("implement me")
+}
+
+func (g Group) DeleteGroup(ctx context.Context, request *pb.DeleteGroupRequest, response *pb.DeleteGroupResponse) error {
+	panic("implement me")
+}
+
 func (g Group) GetGroup(ctx context.Context, request *pb.GetGroupRequest, response *pb.GetGroupResponse) error {
 	db := g.SqlDb
 	rows, err := db.QueryContext(ctx, sql1)
