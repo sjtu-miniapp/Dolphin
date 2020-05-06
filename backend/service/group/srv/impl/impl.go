5a704c7fd7c8d1c61f62f44ff2785f3270e0fef3
blasi <blasi@vip.qq.com>
2020-05-06 18:50:25 +0800

    group rpc

--- backend/service/group/srv/impl/impl.go
+++ backend/service/group/srv/impl/impl.go
@@ -3,6 +3,7 @@ package impl
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/sjtu-miniapp/dolphin/service/group/pb"
 )
 
@@ -40,35 +41,84 @@ func (g Group) GetGroupByUserId(ctx context.Context, request *pb.GetGroupByUserI
 
 func (g Group) CreateGroup(ctx context.Context, request *pb.CreateGroupRequest, response *pb.CreateGroupResponse) error {
 	db := g.SqlDb
-	panic("implement me")
+	result, err := db.ExecContext(ctx, "INSERT INTO `group`(`name`,`creator_id`,`type`) VALUES(?, ?, ?)", request.Name, request.CreatorId, request.Type)
+	if err != nil {
+		return err
+	}
+	if id, err := result.LastInsertId(); err != nil {
+		return err
+	} else {
+		response.Id = uint32(id)
+	}
+	return nil
 }
 
 func (g Group) AddUser(ctx context.Context, request *pb.AddUserRequest, response *pb.AddUserResponse) error {
-	//db := g.SqlDb
-	panic("implement me")
+	db := g.SqlDb
+
+	if len(request.UserIds) == 0 {
+		return nil
+	}
+	sql1 := fmt.Sprintf("INSERT INTO `user_group`(`user_id`, `group_id`) VALUES('%s', %d)", request.UserIds[0], request.GroupId)
+	for _, v := range request.UserIds[1:] {
+		sql1 = sql1 + fmt.Sprintf(", ('%s', %d)", v, request.GroupId)
+	}
+	_, err := db.ExecContext(ctx, sql1)
+	return err
 }
 
+// all values are prepared in api
 func (g Group) UpdateGroup(ctx context.Context, request *pb.UpdateGroupRequest, response *pb.UpdateGroupResponse) error {
-	//db := g.SqlDb
-	panic("implement me")
+	db := g.SqlDb
+	_, err := db.ExecContext(ctx, "UPDATE `group` SET `name`=? WHERE `id`=?", request.Name, request.Id)
+	return err
 }
 
 func (g Group) DeleteGroup(ctx context.Context, request *pb.DeleteGroupRequest, response *pb.DeleteGroupResponse) error {
-	//db := g.SqlDb
-	panic("implement me")
+	db := g.SqlDb
+	_, err := db.ExecContext(ctx, "DELETE FROM `group` WHERE `id`=?",
+		request.Id)
+	return err
 }
 
 func (g Group) GetGroup(ctx context.Context, request *pb.GetGroupRequest, response *pb.GetGroupResponse) error {
-	//db := g.SqlDb
-	//rows, err := db.QueryContext(ctx, sql1)
-	//if err != nil {
-	//	return err
-	//}
-	//if rows.Next() {
-	//	err = rows.Scan(&response.Name)
-	//	if err != nil {
-	//		return err
-	//	}
-	//}
+	db := g.SqlDb
+	errChan := make(chan error)
+	go func() {
+		rows, err := db.QueryContext(ctx, "SELECT `name`, `type`, `creator_id` FROM `group` WHERE `id`=?", request.Id)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		if rows.Next() {
+			err = rows.Scan(&response.Name, &response.Type, &response.CreatorId)
+			errChan <- err
+			if err != nil {
+				return
+			}
+		}
+	}()
+	go func() {
+		rows, err := db.QueryContext(ctx, "SELECT `user`.`id`, `user`.`name` FROM `user_group` JOIN `group` JOIN `user` ON `group_id` = `group`.`id` AND `user_id` = `user`.id WHERE `group_id` = ?", request.Id)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		user := new(pb.User)
+		for rows.Next() {
+			err = rows.Scan(&user.Id, &user.Name)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			response.Users = append(response.Users, user)
+		}
+	}()
+	if <-errChan != nil {
+		return <-errChan
+	}
+	if <-errChan != nil {
+		return <-errChan
+	}
 	return nil
 }
