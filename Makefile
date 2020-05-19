1a6d3a163ae5a660c9628c4dc287c4b53ca2a2c2
blasi <blasi@vip.qq.com>
2020-05-19 20:53:20 +0800

    dockerfile and api

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 SHELL=/bin/bash
 ENV_FILE=.env
 DOCKER_SQL=backend/database/sql
+DOCKER_REDIS=backend/database/redis
 SRV=backend/service
 GOOUT=backend
 GOSCP=backend/scripts
@@ -11,7 +12,7 @@ help:
 source:
 	@cd scripts
 	@source setup.sh local
-build:
+build: clean
 	@version=$$(cat ${API}/VERSION)
 	@source ${GOSCP}/source.sh
 	@SRVLIST=$$(find ${SRV}/* -maxdepth 0 -type d | sed  "s/^.*\///")
@@ -20,15 +21,30 @@ build:
   		if [[ $${srv} == database ]]; then  \
   		continue ; \
 		fi ; \
-  		go build -a -o ../build/$${srv}-srv $${srv}/srv/main.go; \
-  		go build -a -o ../build/$${srv}-api ../api/$${version}/$${srv}/main.go ../api/$${version}/$${srv}/rest.go; \
-  		go build -a -o ../build/$${srv}-cli $${srv}/cli/test.go; \
+  		GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -o ../build/$${srv}-srv $${srv}/srv/main.go; \
+  		GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -o ../build/$${srv}-api ../api/$${version}/$${srv}/main.go ../api/$${version}/$${srv}/rest.go; \
+  		GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -o ../build/$${srv}-cli $${srv}/cli/test.go; \
+  		echo build $${srv}; \
   	done
-docker:
+docker: build
 	@echo $DOCKER_PASSWORD | sudo docker login -u $DOCKER_USERNAME --password-stdin
 	# build all the images
 	@sudo docker build -t nihplod/mysql ${DOCKER_SQL}
 	@sudo docker push nihplod/mysql
+	@sudo docker build -t nihplod/redis ${DOCKER_REDIS}
+	@sudo docker push nihplod/redis
+	@SRVLIST=$$(find ${SRV}/* -maxdepth 0 -type d | sed  "s/^.*\///")
+	@version=$$(cat ${API}/VERSION)
+	@for srv in $${SRVLIST}; do \
+      		if [[ $${srv} == database ]]; then  \
+      		continue ; \
+    		fi ; \
+    		sudo docker build -t nihplod/$${srv}-srv -f ${SRV}/$${srv}/Dockerfile backend; \
+    		sudo docker push nihplod/$${srv}-srv ; \
+    		sudo docker build -t nihplod/$${srv}-api -f ${API}/$${version}/$${srv}/Dockerfile backend; \
+            sudo docker push nihplod/$${srv}-api ; \
+            echo image $${srv} ; \
+    done
 transfer:
 	@#servercnt=$$(sed -n /^SERVER/p ${ENV_FILE}| sed -n '$$p'| cut -b 7)
 	@#for i in $$(seq 1 $${servercnt}); do \
@@ -36,13 +52,13 @@ transfer:
 #		echo "transfer to server$${i}"; \
 #	done
 	@#sshpass -p ${SERVER1_PASSWORD} scp -o StrictHostKeyChecking=no -P ${SERVER1_SSH} -rp * ${SERVER1_USER}@${SERVER1_IP}:~/dolphin
-	@sshpass -p ${SERVER1_PASSWORD} rsync -za -e "ssh -p ${SERVER1_SSH}" . ${SERVER1_USER}@${SERVER1_IP}:~/dolphin
+	@sshpass -p ${SERVER1_PASSWORD} rsync --include='*/' --include='.env' -za -e "ssh -p ${SERVER1_SSH}" . ${SERVER1_USER}@${SERVER1_IP}:~/dolphin
 	@echo "transfer to server1"
 	@#sshpass -p ${SERVER2_PASSWORD} scp -o StrictHostKeyChecking=no -P ${SERVER2_SSH} -rp * ${SERVER2_USER}@${SERVER2_IP}:~/dolphin
-	@sshpass -p ${SERVER2_PASSWORD} rsync -za -e "ssh -p ${SERVER2_SSH}" . ${SERVER2_USER}@${SERVER2_IP}:~/dolphin
+	@sshpass -p ${SERVER2_PASSWORD} rsync --include='*/' --include='.env' -za -e "ssh -p ${SERVER2_SSH}" . ${SERVER2_USER}@${SERVER2_IP}:~/dolphin
 	@echo "transfer to server2"
 	@#sshpass -p ${SERVER3_PASSWORD} scp -o StrictHostKeyChecking=no -P ${SERVER3_SSH} -rp * ${SERVER3_USER}@${SERVER3_IP}:~/dolphin
-	@sshpass -p ${SERVER3_PASSWORD} rsync -za -e "ssh -p ${SERVER3_SSH}" . ${SERVER3_USER}@${SERVER3_IP}:~/dolphin
+	@sshpass -p ${SERVER3_PASSWORD} rsync --include='*/' --include='.env' -za -e "ssh -p ${SERVER3_SSH}" . ${SERVER3_USER}@${SERVER3_IP}:~/dolphin
 	@echo "transfer to server3"
 deploy:	build transfer
 	@sshpass -p ${SERVER1_PASSWORD} ssh -o StrictHostKeyChecking=no -P ${SERVER1_SSH} ${SERVER1_USER}@${SERVER1_IP} 'cd dolphin/scripts && ./setup.sh server'
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,7 +16,7 @@ case "$1" in
 #./server_rest_main -cfg=cfg.yaml
       ;;
     redis)
-      docker pull nihplod/mysql
+      docker pull nihplod/redis
       docker run -dit --name redis -p 6379:6379 nihplod/redis redis-server
       ;;
     sql)
@@ -25,71 +25,9 @@ case "$1" in
       ;;
     etcd)
       /etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://0.0.0.0:2379 --listen-peer-urls http://0.0.0.0:2380 --initial-advertise-peer-urls http://0.0.0.0:2380  --initial-cluster my-etcd-1=http://0.0.0.0:2380
-      ./service --registry=etcd --registry_address=xx.xx.xx.xx:2379
       ;;
     *)
       echo "Invalid option!"
       ;;
 esac
 
-etcd --name cd0 \
---initial-advertise-peer-urls http://192.168.3.45:2380 \
---listen-peer-urls http://192.168.3.45:2380 \
---listen-client-urls http://192.168.3.45:2379 \
---advertise-client-urls http://192.168.3.45:2379 \
---initial-cluster-token etcd-cluster-1 \
---initial-cluster cd0=http://192.168.3.45:2380,cd1=http://192.168.3.45:2480,cd2=http://192.168.3.118:2380 \
---initial-cluster-state new
-etcd --name cd1 \
---initial-advertise-peer-urls http://192.168.3.45:2480 \
---listen-peer-urls http://192.168.3.45:2480 \
---listen-client-urls http://192.168.3.45:2479 \
---advertise-client-urls http://192.168.3.45:2479 \
---initial-cluster-token etcd-cluster-1 \
---initial-cluster cd0=http://192.168.3.45:2380,cd1=http://192.168.3.45:2480,cd2=http://192.168.3.118:2380 \
---initial-cluster-state new
-etcd --name cd2 \
---initial-advertise-peer-urls http://192.168.3.118:2380 \
---listen-peer-urls http://192.168.3.118:2380 \
---listen-client-urls http://192.168.3.118:2379 \
---advertise-client-urls http://192.168.3.118:2379 \
---initial-cluster-token etcd-cluster-1 \
---initial-cluster cd0=http://192.168.3.45:2380,cd1=http://192.168.3.45:2480,cd2=http://192.168.3.118:2380 \
---initial-cluster-state new
-
-etcdctl --endpoints="http://192.168.3.118:2379" endpoint health
-go run main.go --selector=cache --registry=etcdv3 --registry_address=http://192.168.3.45:2479
-go run main.go plugin.go --registry=etcd --registry_address=etcd1.foo.com:2379,etcd2.foo.com:2379,etcd3.foo.com:2379
-
-etcd --name infra0 --initial-advertise-peer-urls http://10.0.1.10:2380 \
-  --listen-peer-urls http://10.0.1.10:2380 \
-  --listen-client-urls http://10.0.1.10:2379,http://127.0.0.1:2379 \
-  --advertise-client-urls http://10.0.1.10:2379 \
-  --initial-cluster-token etcd-cluster-1 \
-  --initial-cluster infra0=http://10.0.1.10:2380,infra1=http://10.0.1.11:2380,infra2=http://10.0.1.12:2380 \
-  --initial-cluster-state new
-
-  $ etcd --name infra1 --initial-advertise-peer-urls http://10.0.1.11:2380 \
-  --listen-peer-urls http://10.0.1.11:2380 \
-  --listen-client-urls http://10.0.1.11:2379,http://127.0.0.1:2379 \
-  --advertise-client-urls http://10.0.1.11:2379 \
-  --initial-cluster-token etcd-cluster-1 \
-  --initial-cluster infra0=http://10.0.1.10:2380,infra1=http://10.0.1.11:2380,infra2=http://10.0.1.12:2380 \
-  --initial-cluster-state new
-
-  etcd --name infra2 --initial-advertise-peer-urls http://10.0.1.12:2380 \
-  --listen-peer-urls http://10.0.1.12:2380 \
-  --listen-client-urls http://10.0.1.12:2379,http://127.0.0.1:2379 \
-  --advertise-client-urls http://10.0.1.12:2379 \
-  --initial-cluster-token etcd-cluster-1 \
-  --initial-cluster infra0=http://10.0.1.10:2380,infra1=http://10.0.1.11:2380,infra2=http://10.0.1.12:2380 \
-  --initial-cluster-state new
-
-
-  etcd --name infra0 --initial-advertise-peer-urls http://localhost:2380 \
-  --listen-peer-urls http://localhost:2380 \
-  --listen-client-urls http://localhost:2379 \
-  --advertise-client-urls http://localhost:2379 \
-  --initial-cluster-token etcd-cluster-1 \
-  --initial-cluster infra0=http://localhost:2380 \
-  --initial-cluster-state new
