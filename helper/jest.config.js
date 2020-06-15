e3a0cbe9227a84d92b61d0f266035eee8c232053
Tony Jin <tonyjin@secondspectrum.com>
2020-06-15 17:06:49 +0800

    Initiazlie helper

---
+++ helper/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '@src/(.*)$': '<rootDir>/src/$1',
+  },
+};
